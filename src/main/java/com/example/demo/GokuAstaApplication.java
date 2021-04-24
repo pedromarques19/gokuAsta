817ba5a3a2f4e3f19ed656c615133281b073ed1e
devpedromarques19 <dev.pedromarques@gmail.com>
2021-04-23 21:44:56 -0300

    Versão Proprieties

--- src/main/java/com/example/demo/GokuAstaApplication.java
+++ src/main/java/com/example/demo/GokuAstaApplication.java
@@ -1,13 +1,30 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GokuAstaApplication {
 
+	@Value ("${numeroAleatorio}")
+	int numeroAleatorio;
+
 	public static void main(String[] args) {
 		SpringApplication.run(GokuAstaApplication.class, args);
 	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner () {
+
+		return args -> {
+			System.out.println(numeroAleatorio);
+		};
+
+
+	}
+
 
 }
